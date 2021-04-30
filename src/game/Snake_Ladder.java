3a8759ce6e09bc7baddd74e3587ec71893a204df
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 21:37:51 +0530

    Added: code for player reaches at 100

--- src/game/Snake_Ladder.java
+++ src/game/Snake_Ladder.java
@@ -6,34 +6,47 @@ public class Snake_Ladder
 	{
 		System.out.println("#####  Welcome to Snake and Ladder Game  #####");
 		System.out.println("\nYou are at Position 0");
-		int dice = (int)(Math.floor((Math.random() * 10 ) % 6) + 1);
-		System.out.println("You Roll: " +dice);
 		
-		final int Ladder = 1;
-		final int Snake = 2;
 		int position = 0;
-		int check = (int)(Math.floor(Math.random() * 10 ) % 3);
 		
-		switch (check)
+		while (position < 100)
 		{
-      		case Ladder:
-      			System.out.println("Player got Ladder");
-      			System.out.println("Player move ahead through ladder");
-      			position += dice;
-      			System.out.println("Now you are at  " +position);
-      			break;
-      		case Snake:
-      			if (position == 0 )
-      				System.out.println("player stays at 0");
-      			else
-      				System.out.println("Player got Snake");
-      				System.out.println("Player move behind through snake");
-      				position -= dice;
-      			break;
-      		default:
-      			if (position <= 0)
-      				position = 0;
-      			System.out.println("Player doesnt move"); break;     			
-		}	
+			int dice = (int)(Math.floor((Math.random() * 10 ) % 6) + 1);
+			System.out.println("\nYou Roll: " +dice);
+			
+			final int Ladder = 1;
+			final int Snake = 2;
+			int check = (int)(Math.floor(Math.random() * 10 ) % 3);		
+			switch (check)
+			{
+      			case Ladder:
+      				System.out.println("Player got Ladder");
+      				position = +position + dice;
+      				if (+position > 100) {
+      					position = +position - dice;
+			        	System.out.println("Now Player1 is at " +position);}
+      				else {
+      					System.out.println("The player1 move head through Ladder");
+			        	System.out.println("Now Player1 is at " +position);}
+      				break;
+      			case Snake:
+      				if (position == 0 )
+      					System.out.println("player stays at 0");
+      				else
+      				{
+      					if (+position > 6) {
+      						position -= dice;
+      						System.out.println("Player got Snake");
+      						System.out.println("Player move behind through snake");
+      						System.out.println("Now you are at  " +position);
+      					}
+      				}
+      				break;
+      			default:
+      				if (position <= 0)
+      					position = 0;
+      					System.out.println("Player doesnt move"); break;     			
+			}
+		}
 	}
 }
