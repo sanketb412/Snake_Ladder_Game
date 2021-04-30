9f143e6e3cde7d6fe1da406f49288b2c885c922d
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 22:04:07 +0530

    Added: code for 2 Players Playing

--- src/game/Snake_Ladder.java
+++ src/game/Snake_Ladder.java
@@ -6,53 +6,84 @@ public class Snake_Ladder
 	{
 		System.out.println("#####  Welcome to Snake and Ladder Game  #####");
 		System.out.println("\nYou are at Position 0");		
-		int position = 0;
-		int countposition = 0;
-		int countdie = 0;
-		while (position < 100)
+		int position1 = 0;
+		int position2 = 0;
+		while (position1 < 100 && position2 < 100)
 		{
-			countdie++;
-			int dice = (int)(Math.floor((Math.random() * 10 ) % 6) + 1);
-			System.out.println("\nYou Roll: " +dice);
+			int dice1 = (int)(Math.floor((Math.random() * 10 ) % 6) + 1);
+			System.out.println("\nYou Roll: " +dice1);
 			
-			final int Ladder = 1;
-			final int Snake = 2;
-			int check = (int)(Math.floor(Math.random() * 10 ) % 3);		
-			switch (check)
+			int check1 = (int)(Math.floor(Math.random() * 10 ) % 3);		
+			switch (check1)
 			{
-      			case Ladder:
-      				System.out.println("Player got Ladder");
-      				position = +position + dice;
-      				countposition++;
-      				if (+position > 100) {
-      					position = +position - dice;
-			        	System.out.println("Now Player is at " +position);}
+      			case 1:
+      				System.out.println("Player1 got Ladder");
+      				position1 = +position1 + dice1;
+      				if (+position1 > 100) {
+      					position1 = +position1 - dice1;
+			        	System.out.println("Now Player1 is at " +position1);}
       				else {
-      					System.out.println("The player move head through Ladder");
-			        	System.out.println("Now Player is at " +position);}
+      					System.out.println("The player1 move head through Ladder");
+			        	System.out.println("Now Player1 is at " +position1);}
       				break;
-      			case Snake:
-      				if (position == 0 )
+      			case 2:
+      				if (position1 == 0 )
       					System.out.println("player stays at 0");
       				else
       				{
-      					if (+position > 6) {
-      						position -= dice;
-      						countposition++;
-      						System.out.println("Player got Snake");
-      						System.out.println("Player move behind through snake");
-      						System.out.println("Now you are at  " +position);
+      					if (+position1 > 6) {
+      						position1 -= dice1;
+      						System.out.println("Player1 got Snake");
+      						System.out.println("Player1 move behind through snake");
+      						System.out.println("Now you are at  " +position1);
       					}
       				}
       				break;
       			default:
-      				if (position <= 0)
-      					position = 0;
-      					System.out.println("Player doesnt move"); break;     			
+      				if (position1 <= 0)
+      					position1 = 0;
+      					System.out.println("Player1 doesnt move"); break;     			
 			}
-			System.out.println("\n***Player Wins***");
-			System.out.println("\nThe Position of a Player had change " +countposition +" times");
+			int dice2 = (int)(Math.floor((Math.random() * 10 ) % 6) + 1);
+			System.out.println("\nYou Roll: " +dice2);
+			
+			int check2 = (int)(Math.floor(Math.random() * 10 ) % 3);		
+			switch (check2)
+			{
+      			case 1:
+      				System.out.println("Player2 got Ladder");
+      				position2 = +position2 + dice2;
+      				if (+position2 > 100) {
+      					position2 = +position2 - dice2;
+			        	System.out.println("Now Player2 is at " +position2);}
+      				else {
+      					System.out.println("The player2 move head through Ladder");
+			        	System.out.println("Now Player2 is at " +position2);}
+      				break;
+      			case 2:
+      				if (position2 == 0 )
+      					System.out.println("player2 stays at 0");
+      				else
+      				{
+      					if (+position2 > 6) {
+      						position2 -= dice2;
+      						System.out.println("Player2 got Snake");
+      						System.out.println("Player2 move behind through snake");
+      						System.out.println("Now you are at  " +position2);
+      					}
+      				}
+      				break;
+      			default:
+      				if (position2 <= 0)
+      					position2 = 0;
+      					System.out.println("Player2 doesnt move"); break;     			
+			}
 		}
-		System.out.println("\nThe Dice was Rolled " +countdie +" times");
+		System.out.println("\nThe Final Position of Player1 is: " +position1);
+        System.out.println("\nThe Final Position of Player2 is: " +position2);
+        if (position1 == 100)
+       	System.out.println("\n####...CONGRATULATION.!!!! PLAYER1 has WON THE GAME...####");
+        else
+           System.out.println("\n####...CONGRATULATION.!!!! PLAYER2 has WON THE GAME...####");
 	}
 }
